23ea19a8bea67afd6d025e63231ffae9d6cf6db5
Leslie <mr.feeling.heart@gmail.com>
2021-07-05 22:44:02 +0800

    update

--- app/src/main/java/com/leslie/annotation/MainActivity.java
+++ app/src/main/java/com/leslie/annotation/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.leslie.task_annotation.Task;
+
 
 @Task
 public class MainActivity extends AppCompatActivity {
